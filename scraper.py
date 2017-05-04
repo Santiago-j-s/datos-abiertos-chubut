12972f24b74d5c8a13254df970cad50200eec738
Santiago Santana <santana.santiago@gmail.com>
2017-05-04 08:30:35 -0300

    Refactorización scraper.py

--- scraper.py
+++ scraper.py
@@ -1,22 +1,30 @@
+#!/usr/bin/env python3
 import requests
 import json
 
+base_url = 'http://datos.chubut.gov.ar/api/3'
+urls = {
+    'package_list': ''.join([base_url, '/action/package_list']),
+    'package_show': ''.join([base_url, '/action/package_show']),
+}
+
 s = requests.Session()
-response = s.get('http://datos.chubut.gov.ar/api/3/action/package_list')
+response = s.get(urls['package_list'])
 
 package_list = response.json()['result']
 
 for package in package_list:
-    package_data = s.get(''.join(['http://datos.chubut.gov.ar/api/3/action/package_show?id=', package]))
+    package_data = s.get(''.join([urls['package_show'], '?id=', package]))
     recursos = package_data.json()['result']['resources']
     for recurso in recursos:
-        filename = 'data/'
-        if(recurso['format'].lower() in recurso['name'].lower()):
-            filename += recurso['name']
-        else:
-            filename += ''.join([recurso['name'], '.', recurso['format'].lower()])
+        folder = 'data/'
+        filename = ''.join([folder, recurso['name']])
+        
+        if(recurso['format'].lower() not in recurso['name'].lower()):
+            filename = ''.join([filename, ".", recurso['format'].lower()])
+        
         r = s.get(recurso['url'])
-        with open(filename, 'xb')  as f:
+        with open(filename, 'xb') as f:
             for chunk in r.iter_content(chunk_size=512*1024):
                 if chunk:
                     f.write(chunk)
